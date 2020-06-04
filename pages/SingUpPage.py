708a6a11c94208552f3f505f8c351f4642ecee46
QA <d.bulka@pixelplex.io>
2020-06-04 20:17:23 +0300

    add sing_up test

--- pages/SingUpPage.py
+++ pages/SingUpPage.py
@@ -4,9 +4,15 @@ from selenium.webdriver.common.by import By
 
 class SingUpPage(BasePage):
     sing_up_url = BasePage.base_url + "password-create"
-    create_password_locator = (By.XPATH, "//input[@name='password']")
-    confirm_password_locator = (By.XPATH, "//input[@name='repeatPassword']")
-    create_password_button_locator = (By.XPATH, "//button[@class='ui basic button main-btn']")
+    account_name_locator = (By.XPATH, "//input[@name='accountName']")
+    generated_wif_locator = (By.XPATH, "//input[@name='generatedWIF']")
+    confirm_wif_locator = (By.XPATH, "//input[@name='confirmWIF']")
+    lose_wif_label_locator = (By.XPATH, "//*[@class='label' and @for='1']")
+    recover_wif_label_locator = (By.XPATH, "//*[@class='label' and @for='2']")
+    store_wif_label_locator = (By.XPATH, "//*[@class='label' and @for='3']")
+    create_account_button_locator = (
+        By.XPATH, "//button[@type='submit' and @class='ui basic button main-btn fix-width']")
+    account_name = "bigD"
 
     def open_page(self):
         super().navigate_to(self.sing_up_url)
@@ -16,10 +22,12 @@ class SingUpPage(BasePage):
         super().get_element(locator).send_keys(text)
         return SingUpPage(self.driver)
 
-    def press_button(self, locator):
+    def click_element(self, locator):
         super().get_element(locator).click()
 
     def follow_link(self, locator):
         super().get_element(locator).click()
 
-
+    def get_field_data(self, locator):
+        text = super().get_element(locator).get_attribute('value')
+        return text
--- ui_tests/test_sing_up.py
+++ ui_tests/test_sing_up.py
@@ -9,12 +9,20 @@ from pages.SingUpPage import SingUpPage
 @pytest.mark.sing_up
 class TestSingUp:
 
-    def test_sing_in(self, driver):
+    def test_sing_up(self, driver):
         browser = SingUpPage(driver)
         browser.open_page()
         browser.fill_field(locator=CreatePasswordPage.create_password_locator, text=CreatePasswordPage.password)
         browser.fill_field(locator=CreatePasswordPage.confirm_password_locator, text=CreatePasswordPage.password)
-        browser.press_button(locator=CreatePasswordPage.create_password_button_locator)
+        browser.click_element(locator=CreatePasswordPage.create_password_button_locator)
         time.sleep(1)
         browser.follow_link(locator=SingInPage.sing_up_locator)
         time.sleep(1)
+        browser.fill_field(locator=SingUpPage.account_name_locator, text=SingUpPage.account_name)
+        generated_wif = browser.get_field_data(SingUpPage.generated_wif_locator)
+        browser.fill_field(locator=SingUpPage.confirm_wif_locator, text=generated_wif)
+        browser.click_element(locator=SingUpPage.lose_wif_label_locator)
+        browser.click_element(locator=SingUpPage.recover_wif_label_locator)
+        browser.click_element(locator=SingUpPage.store_wif_label_locator)
+        browser.click_element(locator=SingUpPage.create_account_button_locator)
+        time.sleep(5)
