43c82560070f647a6e53c61fec735674c7e24084
QA <d.bulka@pixelplex.io>
2020-06-05 12:40:26 +0300

    add firefox browser testing

--- conftest.py
+++ conftest.py
@@ -1,12 +1,27 @@
 import pytest
+import json
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture
-def driver():
-    driver_manager = ChromeDriverManager()
-    driver = webdriver.Chrome(executable_path=driver_manager.install())
-    driver.implicitly_wait(10)
+def config():
+    with open('ui_tests/config.json') as config_file:
+        data = json.load(config_file)
+        return data
+
+
+@pytest.fixture
+def driver(config):
+    if config["browser"] == "chrome":
+        driver_manager = ChromeDriverManager()
+        driver = webdriver.Chrome(executable_path=driver_manager.install())
+    elif config["browser"] == "firefox":
+        driver_manager = GeckoDriverManager()
+        driver = webdriver.Firefox(executable_path=driver_manager.install())
+    else:
+        raise Exception(f'"{config["browser"]}" is not a supported browser')
+    driver.implicitly_wait(config['wait_time'])
     yield driver
     driver.quit()
